86a995d1b47a96abb23b4c0067b412d7685401d4
Develokev <develokev@gmail.com>
2023-03-06 13:03:36 +0100

    first commit K

---
+++ app.js
@@ -0,0 +1,15 @@
+const {arrayColores, arrayColores2} = require('./colores');
+
+arrayColores.forEach((item) => {
+    console.log(item);
+})
+
+arrayColores2.forEach((item) => {
+    console.log(item);
+})
+
+//npm START para inicializar el NODE
+
+//npm RUN TEST, para inicializar el TEST
+
+//
---
+++ colores.js
@@ -0,0 +1,7 @@
+const arrayColores = ['rojo', 'verde', 'azul'];
+const arrayColores2 = ['cyan', 'magenta', 'amarillo'];
+
+module.exports = {
+    arrayColores,
+    arrayColores2
+}
